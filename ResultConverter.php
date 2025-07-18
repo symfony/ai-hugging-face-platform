94655aefcade97743e74b7964c908fc26f7a8c27
Christopher Hertel <mail@christopher-hertel.de>
2025-07-18 11:15:56 +0200

    Rework Platform API to invoke and result

--- ResultConverter.php
+++ ResultConverter.php
@@ -23,29 +23,29 @@ use Symfony\AI\Platform\Bridge\HuggingFace\Output\ZeroShotClassificationResult;
 use Symfony\AI\Platform\Exception\InvalidArgumentException;
 use Symfony\AI\Platform\Exception\RuntimeException;
 use Symfony\AI\Platform\Model;
-use Symfony\AI\Platform\Response\BinaryResponse;
-use Symfony\AI\Platform\Response\ObjectResponse;
-use Symfony\AI\Platform\Response\RawHttpResponse;
-use Symfony\AI\Platform\Response\RawResponseInterface;
-use Symfony\AI\Platform\Response\ResponseInterface;
-use Symfony\AI\Platform\Response\TextResponse;
-use Symfony\AI\Platform\Response\VectorResponse;
-use Symfony\AI\Platform\ResponseConverterInterface as PlatformResponseConverter;
+use Symfony\AI\Platform\Result\BinaryResult;
+use Symfony\AI\Platform\Result\ObjectResult;
+use Symfony\AI\Platform\Result\RawHttpResult;
+use Symfony\AI\Platform\Result\RawResultInterface;
+use Symfony\AI\Platform\Result\ResultInterface;
+use Symfony\AI\Platform\Result\TextResult;
+use Symfony\AI\Platform\Result\VectorResult;
+use Symfony\AI\Platform\ResultConverterInterface as PlatformResponseConverter;
 use Symfony\AI\Platform\Vector\Vector;
 
 /**
  * @author Christopher Hertel <mail@christopher-hertel.de>
  */
-final readonly class ResponseConverter implements PlatformResponseConverter
+final readonly class ResultConverter implements PlatformResponseConverter
 {
     public function supports(Model $model): bool
     {
         return true;
     }
 
-    public function convert(RawResponseInterface|RawHttpResponse $response, array $options = []): ResponseInterface
+    public function convert(RawResultInterface|RawHttpResult $result, array $options = []): ResultInterface
     {
-        $httpResponse = $response->getRawObject();
+        $httpResponse = $result->getObject();
         if (503 === $httpResponse->getStatusCode()) {
             return throw new RuntimeException('Service unavailable.');
         }
@@ -72,25 +72,25 @@ final readonly class ResponseConverter implements PlatformResponseConverter
         $task = $options['task'] ?? null;
 
         return match ($task) {
-            Task::AUDIO_CLASSIFICATION, Task::IMAGE_CLASSIFICATION => new ObjectResponse(
+            Task::AUDIO_CLASSIFICATION, Task::IMAGE_CLASSIFICATION => new ObjectResult(
                 ClassificationResult::fromArray($content)
             ),
-            Task::AUTOMATIC_SPEECH_RECOGNITION => new TextResponse($content['text'] ?? ''),
-            Task::CHAT_COMPLETION => new TextResponse($content['choices'][0]['message']['content'] ?? ''),
-            Task::FEATURE_EXTRACTION => new VectorResponse(new Vector($content)),
-            Task::TEXT_CLASSIFICATION => new ObjectResponse(ClassificationResult::fromArray(reset($content) ?? [])),
-            Task::FILL_MASK => new ObjectResponse(FillMaskResult::fromArray($content)),
-            Task::IMAGE_SEGMENTATION => new ObjectResponse(ImageSegmentationResult::fromArray($content)),
-            Task::IMAGE_TO_TEXT, Task::TEXT_GENERATION => new TextResponse($content[0]['generated_text'] ?? ''),
-            Task::TEXT_TO_IMAGE => new BinaryResponse($content, $contentType),
-            Task::OBJECT_DETECTION => new ObjectResponse(ObjectDetectionResult::fromArray($content)),
-            Task::QUESTION_ANSWERING => new ObjectResponse(QuestionAnsweringResult::fromArray($content)),
-            Task::SENTENCE_SIMILARITY => new ObjectResponse(SentenceSimilarityResult::fromArray($content)),
-            Task::SUMMARIZATION => new TextResponse($content[0]['summary_text']),
-            Task::TABLE_QUESTION_ANSWERING => new ObjectResponse(TableQuestionAnsweringResult::fromArray($content)),
-            Task::TOKEN_CLASSIFICATION => new ObjectResponse(TokenClassificationResult::fromArray($content)),
-            Task::TRANSLATION => new TextResponse($content[0]['translation_text'] ?? ''),
-            Task::ZERO_SHOT_CLASSIFICATION => new ObjectResponse(ZeroShotClassificationResult::fromArray($content)),
+            Task::AUTOMATIC_SPEECH_RECOGNITION => new TextResult($content['text'] ?? ''),
+            Task::CHAT_COMPLETION => new TextResult($content['choices'][0]['message']['content'] ?? ''),
+            Task::FEATURE_EXTRACTION => new VectorResult(new Vector($content)),
+            Task::TEXT_CLASSIFICATION => new ObjectResult(ClassificationResult::fromArray(reset($content) ?? [])),
+            Task::FILL_MASK => new ObjectResult(FillMaskResult::fromArray($content)),
+            Task::IMAGE_SEGMENTATION => new ObjectResult(ImageSegmentationResult::fromArray($content)),
+            Task::IMAGE_TO_TEXT, Task::TEXT_GENERATION => new TextResult($content[0]['generated_text'] ?? ''),
+            Task::TEXT_TO_IMAGE => new BinaryResult($content, $contentType),
+            Task::OBJECT_DETECTION => new ObjectResult(ObjectDetectionResult::fromArray($content)),
+            Task::QUESTION_ANSWERING => new ObjectResult(QuestionAnsweringResult::fromArray($content)),
+            Task::SENTENCE_SIMILARITY => new ObjectResult(SentenceSimilarityResult::fromArray($content)),
+            Task::SUMMARIZATION => new TextResult($content[0]['summary_text']),
+            Task::TABLE_QUESTION_ANSWERING => new ObjectResult(TableQuestionAnsweringResult::fromArray($content)),
+            Task::TOKEN_CLASSIFICATION => new ObjectResult(TokenClassificationResult::fromArray($content)),
+            Task::TRANSLATION => new TextResult($content[0]['translation_text'] ?? ''),
+            Task::ZERO_SHOT_CLASSIFICATION => new ObjectResult(ZeroShotClassificationResult::fromArray($content)),
 
             default => throw new RuntimeException(\sprintf('Unsupported task: %s', $task)),
         };
